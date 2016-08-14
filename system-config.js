6f6744d29e0a6d59c54ba95841c71db4628de3db
Sonny <sk222sw@student.lnu.se>
2016-08-14 19:23:11 +0200

    new gh-pages version

--- system-config.js
+++ system-config.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 // SystemJS configuration file, see links for more information
 // https://github.com/systemjs/systemjs
 // https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
@@ -6,9 +6,16 @@
  * User Configuration.
  **********************************************************************************************/
 /** Map relative paths to URLs. */
-var map = {};
+var map = {
+    'angular2-google-maps': 'vendor/angular2-google-maps',
+};
 /** User packages configuration. */
-var packages = {};
+var packages = {
+    'angular2-google-maps/core': {
+        defaultExtension: 'js',
+        main: 'index.js',
+    },
+};
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
  * Everything underneath this line is managed by the CLI.
@@ -28,6 +35,15 @@ var barrels = [
     // App specific barrels.
     'app',
     'app/shared',
+    'app/test',
+    'app/log-in',
+    'app/profile',
+    'app/home',
+    'app/flash',
+    'app/map',
+    'app/map-cmp',
+    'app/theft-list',
+    'app/theft-info',
 ];
 var cliSystemConfigPackages = {};
 barrels.forEach(function (barrelName) {
@@ -38,9 +54,9 @@ System.config({
     map: {
         '@angular': 'vendor/@angular',
         'rxjs': 'vendor/rxjs',
-        'main': 'main.js'
+        'main': 'main.js',
     },
-    packages: cliSystemConfigPackages
+    packages: cliSystemConfigPackages,
 });
 // Apply the user's configuration.
 System.config({ map: map, packages: packages });
